6d480795bb5e61702d4e4876390fb2db7e210ba0
RohanRao1 <74089730+RohanRao1@users.noreply.github.com>
2023-07-03 17:22:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return <Fragment>
+    <Header />
+  </Fragment>
+}
+
+export default App;
